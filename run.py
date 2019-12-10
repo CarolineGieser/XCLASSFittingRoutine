de679155dad409ea80396e1e6560c3a938b2e787
Caroline Gieser <gieser@mpia.de>
2019-12-10 11:53:08 +0100

    update

--- run.py
+++ run.py
@@ -1,19 +1,17 @@
-import functs as fc
-
-
+from Functions import functs as fc
 
 ### LOADING INPUT AND SETTING UP DIRECTORIES ###
 #setup working directory
-working_directory = fc.setup_directory(delete_previous_results=False)
+working_directory = fc.setup_directory(delete_previous_results=True)
 
-#fc.determine_noise()
+fc.determine_noise()
 
-#fc.extract_spectrum_init()
-#fc.setup_XCLASS_files(working_directory)
-#fc.run_XCLASS_fit()
+fc.extract_spectrum_init()
+fc.setup_XCLASS_files(working_directory)
+fc.run_XCLASS_fit()
 
 
 fc.extract_results(plotting=True)
 fc.create_plots()
-#fc.run_XCLASS_fit_all_fixed(working_directory)
+fc.run_XCLASS_fit_all_fixed(working_directory)
 fc.plot_fit()
